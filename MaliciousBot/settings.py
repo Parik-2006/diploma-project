87d1ca8291e5d4efe4a0b2a7192048ae4ed97e4d
Parik-2006 <parikshithbb.cs25@rvce.edu.in>
2026-04-08 19:43:39 +0530

    Fix staticfiles storage backend for Render deploy

--- MaliciousBot/settings.py
+++ MaliciousBot/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     }
 }
 
